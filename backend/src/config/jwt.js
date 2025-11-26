0dade7a22d90c8ed023d217126138f7c783a5dbe
jaydeep kumar <118693035+Jaydeepkumar0p@users.noreply.github.com>
2025-11-26 12:41:53 +0530

    Update jwt.js

--- backend/src/config/jwt.js
+++ backend/src/config/jwt.js
@@ -5,10 +5,9 @@ export const genrateToken=(res,userId)=>{
         expiresIn:'30d',
     })
     res.cookie('jwt',token,{
-        httponly:true,
-        secure:process.env.NODE_ENV !=='development',
-        sameSite:'strict',
-        maxAge:30*24*60*60*1000, // 30 days
+        httpOnly: true,
+  secure: true,       
+  sameSite: "none",
     })
     return token;
 }
